0c2d107d772d596af23ad3251bd45fa2bf3c7dc2
AswinKaruppiah <aswinthkaruppiah@gmail.com>
2023-04-10 19:16:30 +0530

    rechange

--- src/Ui/Intro/Intro.js
+++ src/Ui/Intro/Intro.js
@@ -7,11 +7,11 @@ import "react-html5video/dist/styles.css";
 function Intro() {
   return (
     <div className="intro">
-      <Video autoPlay loop muted controls={false}>
+      {/* <Video autoPlay loop muted controls={false}>
         <source src={intro} type="video/webm" />
         />
-      </Video>
-      {/* <video src={intro} autoplay="true" loop muted type="video/mp4" /> */}
+      </Video> */}
+      <video src={intro} autoplay="true" loop muted type="video/mp4" />
     </div>
   );
 }
