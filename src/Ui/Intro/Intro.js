ea5505a49fc41dd19e3f15b11eb3049b870ddcf0
AswinKaruppiah <aswinthkaruppiah@gmail.com>
2023-04-10 19:44:34 +0530

    vi

--- src/Ui/Intro/Intro.js
+++ src/Ui/Intro/Intro.js
@@ -1,23 +1,16 @@
-import React, { useRef, useEffect } from "react";
+import React from "react";
 import "./Intro.css";
 import intro from "./CodeFest design.4k (2).mp4";
-// import { DefaultPlayer as Video } from "react-html5video";
-// import "react-html5video/dist/styles.css";
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
 
 function Intro() {
-  const videoRef = useRef(undefined);
-  useEffect(() => {
-    videoRef.current.defaultMuted = true;
-  });
   return (
     <div className="intro">
-      <video ref={videoRef} loop autoPlay muted playsInline>
-        <source src={intro} type="video/mp4" />
-      </video>
-      {/* <Video autoPlay loop muted controls={false}>
+      <Video autoPlay loop muted controls={false}>
         <source src={intro} type="video/webm" />
         />
-      </Video> */}
+      </Video>
       {/* <video controls src={intro} autoplay="true" loop muted type="video/mp4" /> */}
     </div>
   );
