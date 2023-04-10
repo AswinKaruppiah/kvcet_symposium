b3d99cafe1da4da03474cab3951fdbf878d8de18
AswinKaruppiah <aswinthkaruppiah@gmail.com>
2023-04-10 19:04:37 +0530

    video bug

--- src/Ui/Intro/Intro.js
+++ src/Ui/Intro/Intro.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Intro.css";
 import intro from "./CodeFest design.4k (2).mp4";
+import { DefaultPlayer as Video } from "react-html5video";
+import "react-html5video/dist/styles.css";
 
 function Intro() {
   return (
     <div className="intro">
-      <video src={intro} autoplay="true" loop muted type="video/mp4" />
+      <Video autoPlay loop muted controls={false}>
+        <source src={intro} type="video/webm" />
+        />
+      </Video>
+      {/* <video src={intro} autoplay="true" loop muted type="video/mp4" /> */}
     </div>
   );
 }
